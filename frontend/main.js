ecbe3f3833dba9efd216dab7f562386f5afd499d
Wesley <123242004+Wesley-Programers@users.noreply.github.com>
2025-10-24 18:57:03 -0300

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,17 @@
+window.addEventListener("DOMContentLoaded", async () => {
+    try {
+        const res = await fetch("", {
+            credentials: "include"
+        })
 
+        if (!res.ok) {
+            console.log("");
+        }
+
+        const data = await res.json();
+        console.log(data)
+
+    } catch (error) {
+        console.error("", error)
+    }
+})
