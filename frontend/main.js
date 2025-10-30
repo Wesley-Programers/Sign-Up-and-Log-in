d161e2ecc037e85f2923e4af5381efc2c6bc934b
Wesley <123242004+Wesley-Programers@users.noreply.github.com>
2025-10-30 19:25:58 -0300

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,15 +1,36 @@
-const themeOption = document.querySelectorAll('input[name="themes"]');
-themeOption.forEach(themeOption => {
-    themeOption.addEventListener("change", (event) => {
+document.querySelectorAll('input[name="themes"]').forEach((input) => {
+    input.addEventListener("change", (event) => {
         const theme = event.target.value;
         console.log("Escolhido: ", theme);
 
+        body.classList.remove("light", "dark");
+        body.classList.add(theme);
+
         localStorage.setItem("currentTheme", theme);
-    });
+    })
+});
+
+
+const getTheme = localStorage.getItem("currentTheme");
+const body = document.body;
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    if (getTheme) {
+        body.classList.add(getTheme);
+        const currentTheme = document.querySelector(`input[name="theme"][value="${theme}"]`)
+        if (currentTheme) {
+            currentTheme.checked = true;
+        } else {
+            body.classList.add("light");
+        }
+    };
+
 });
 
 
 window.addEventListener("DOMContentLoaded", async () => {
+
     try {
         const res = await fetch("", {
             credentials: "include"
@@ -23,8 +44,9 @@ window.addEventListener("DOMContentLoaded", async () => {
         console.log(data);
         console.log(data.name);
         console.log(data.email);
+        welcome.innerHTML = ``
 
     } catch (error) {
-        console.error("", error);
+        console.error("", error)
     }
 });
