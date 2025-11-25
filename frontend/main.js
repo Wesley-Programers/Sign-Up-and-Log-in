7b02f2346d3f85d086e6057ac7f69bfcc21f908c
Wesley <123242004+Wesley-Programers@users.noreply.github.com>
2025-11-25 15:51:53 -0300

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,184 +1,132 @@
-let settings = document.getElementById("setting");
-let welcome = document.getElementById("welcome");
-let account = document.getElementById("account");
-let theme = document.getElementById("theme");
-let security = document.getElementById("security");
-let language = document.getElementById("language");
-
-let darkTheme = document.getElementById("darkTheme");
-let lightTheme = document.getElementById("lightTheme");
-let dark = document.getElementById("dark");
-let light = document.getElementById("light");
-
-let portugueseLanguage = document.getElementById("portugueseLanguage");
-let englishLanguage = document.getElementById("englishLanguage");
-let portuguese = document.getElementById("portuguese");
-let english = document.getElementById("english");
+function alreadyHaveAccount() {
+    setTimeout(() => {
+        window.location.href = '../HTML/logIn.html'
+    }, 250);
+};
 
 
-document.querySelectorAll('input[name="languages"]').forEach((input) => {
-    
-    input.addEventListener("change", (event) => {
-        const language = event.target.value;
-        console.log("Escolhido: ", language);
+document.addEventListener("DOMContentLoaded", () => {
 
-        body.classList.remove("english", "portuguese");
-        body.classList.add(language);
-
-        localStorage.setItem("currentLanguage", language);
-        localStorage.setItem("currentLanguage", language);
-        setTimeout(() => {
-            window.location.href = window.location.href;
-        }, 300);
-    });
-});
+    let form = document.getElementById("form-sign-up");
 
 
-document.querySelectorAll('input[name="themes"]').forEach((input) => {
-    
-    input.addEventListener("change", (event) => {
-        const theme = event.target.value;
-        console.log("Escolhido: ", theme);
+    form.addEventListener("submit", async (a) => {
+        a.preventDefault();
 
-        body.classList.remove("light", "dark");
-        body.classList.add(theme);
+        const formData = new FormData(a.target)
+        let thisNameAlreadyExists = document.getElementById('nameAlreadyExits');
+        let thisEmailAlreadyExists = document.getElementById('emailAlreadyExits');
+        
+        try {
 
-        localStorage.setItem("currentTheme", theme);
-        localStorage.setItem("currentLanguage", language);
-        setTimeout(() => {
-            window.location.href = window.location.href;
-        }, 300);
-    });
-});
+            const fetchAqui = await fetch("", {
+                method: "POST",
+                body: formData
+            })
 
+            const emailData = document.getElementById('inputEmail').value
+            let passwordValue = document.getElementById('inputPassword').value
+            let incorretEmail = document.getElementById('emailIncorrect')
+            let shortPassword = document.getElementById('shortPassword')
 
-const getTheme = localStorage.getItem("currentTheme");
-const getLanguage = localStorage.getItem("currentLanguage");
-const body = document.body;
+            const status = fetchAqui.status
+            const mensagem = await fetchAqui.text()
+            alert(`Status: ${status} Mensagem: ${mensagem}`)
+            
+            let passwordLength = passwordValue.length >= 8;
+            // let teste = emailData.includes("@email.com");
 
+            if (teste && passwordLength && status === 201 && mensagem === "Dados validos") {
+                console.log("EVERYTHING IS ALRIGHT");
+                setTimeout(() => {
+                    window.location.href = '../HTML/mainAccount.html'
+                }, 500);
+            } else {
+                a.preventDefault();
+                console.log("something is wrong");
 
-window.addEventListener("DOMContentLoaded", () => {
-    
-    console.log("classes atuais: ", body.classList);
+                if (!teste) {
+                    console.log("email nao valido");
+                    incorretEmail.style.display = 'block';
+                } 
 
-    if (getTheme) {
-        body.classList.add(getTheme);
-        const currentTheme = document.querySelector(`input[name="theme"][value="${getTheme}"]`);
-        if (currentTheme) {
-            currentTheme.checked = true;
-        } else {
-            body.classList.add("light");
+                if (!passwordLength) {
+                    console.log("senha curta");
+                    shortPassword.style.display = 'block';
+                }
+
+                if (status === 409 && mensagem === "Nome já existe") {
+                    console.log("nome ja existe")
+                    thisNameAlreadyExists.style.display = 'block';  
+                }
+
+                if (status === 409 && mensagem === "Email já existe") {
+                    console.log("email ja existe")
+                    thisEmailAlreadyExists.style.display = 'block';
+                }
+
+            }
+
+        } catch ( error ) {
+            console.error("ERROR: ", error);
+            alert("SOME ERROR");
         };
+
+    });
+
+});
+
+
+function newLanguage() {
+    
+    let languagesOption = document.getElementById('languages');
+    let languages = languagesOption.value;
+
+    let emailNotValid = document.getElementById('emailIncorrect');
+    let shortPassword = document.getElementById('shortPassword');
+    let nameAlreadyExits = document.getElementById('nameAlreadyExits');
+    let emailAlreadyExits = document.getElementById('emailAlreadyExits');
+    let englishLanguage = document.getElementById('englishLanguage');
+    let portugueseLanguage = document.getElementById('portugueseLanguage');
+    let welcome = document.getElementById('welcome');
+    let title = document.getElementById('sign-up-title');
+    let name = document.getElementById('inputName');
+    let email = document.getElementById('inputEmail');
+    let password = document.getElementById('inputPassword');
+    let withoutAccount = document.getElementById('withoutAccount');
+    let send = document.getElementById('send');
+
+    if (languages === "portuguese") {
+
+        welcome.innerHTML = 'BEM-VINDO';
+        title.innerHTML = 'CADASTRE-SE';
+        withoutAccount.innerHTML = 'Já tem uma conta?';
+        send.innerHTML = 'Cadastrar-se';
+        portugueseLanguage.innerHTML = 'Português';
+        englishLanguage.innerHTML = 'Inglês';
+        name.placeholder = 'Nome';
+        email.placeholder = 'Email';
+        password.placeholder = 'Senha';
+        nameAlreadyExits.innerHTML = 'Esse nome já está em uso';
+        emailAlreadyExits.innerHTML = 'Esse email já esta em uso';
+        shortPassword.innerHTML = 'Sua senha é muito fraca';
+        emailNotValid.innerHTML = 'Esse email não é valido';
+
+    } else if (languages === "english") {
+
+        welcome.innerHTML = 'WELCOME';
+        title.innerHTML = 'SIGN UP';
+        withoutAccount.innerHTML = "Already have an account?";
+        send.innerHTML = 'Sign up';
+        portugueseLanguage.innerHTML = 'Portuguese';
+        englishLanguage.innerHTML = 'English';
+        name.placeholder = 'Name';
+        email.placeholder = 'Email';
+        password.placeholder = 'Password';
+        nameAlreadyExits.innerHTML = 'This name already exists';
+        emailAlreadyExits.innerHTML = 'This email already exists';
+        shortPassword.innerHTML = 'Your password is so weak';
+        emailNotValid.innerHTML = 'Enter a valid email adress';
+
     };
-
-    if (getLanguage) {
-        body.classList.add(getLanguage);
-        const currentLanguage = document.querySelector(`input[name="languages"][value="${getLanguage}"]`);
-        if (currentLanguage) {
-            currentLanguage.checked = true;
-        } else {
-            body.classList.add("portuguese")
-            console.log("portugues adicionado");
-        };
-    };
-
-    if (body.classList.contains("portuguese")) {
-        console.log("usuario prefere portugues");
-
-        settings.innerHTML = '';
-        welcome.innerHTML =  '';
-        account.innerHTML = '';
-        theme.innerHTML = '';
-        language.innerHTML = '';
-        security.innerHTML = '';
-        dark.innerHTML = '';
-        light.innerHTML = '';
-        english.innerHTML = '';
-        portuguese.innerHTML = '';
-    } else if (body.classList.contains("english")) {
-        console.log("usuario prefere ingles");
-
-        settings.innerHTML = '';
-        welcome.innerHTML =  '';
-        account.innerHTML = '';
-        theme.innerHTML = '';
-        language.innerHTML = '';
-        security.innerHTML = '';
-        dark.innerHTML = '';
-        light.innerHTML = '';
-        english.innerHTML = '';
-        portuguese.innerHTML = '';
-    };
-
-});
-
-
-window.addEventListener("DOMContentLoaded", async () => {
-
-    try {
-        const res = await fetch("", {
-            credentials: "include"
-        })
-
-        if (!res.ok) {
-            console.log("nao logado");
-        }
-
-        const data = await res.json();
-        console.log(data);
-        console.log(data.name);
-        console.log(data.email);
-        welcome.innerHTML = `HELLO, HOW'S IT GOING ${data.name}?`
-
-    } catch (error) {
-        console.error("Erro ao carregar usuario: ", error)
-    }
-})
-
-
-settings.addEventListener("click", () => {
-    settings.style.display = 'none';
-
-    welcome.style.display = 'none';
-    account.style.display = 'block';
-    theme.style.display = 'block';
-    language.style.display = 'block';
-    security.style.display = 'block';
-});
-
-
-theme.addEventListener("click", () => {
-    settings.style.display = 'none';
-
-    account.style.display = 'none';
-    theme.style.display = 'none';
-    language.style.display = 'none';
-    security.style.display = 'none';
-
-
-    dark.style.display = 'block';
-    light.style.display = 'block';
-    darkTheme.style.display = 'block';
-    lightTheme.style.display = 'block';
-});
-
-
-account.addEventListener("click", () => {
-    account.style.display = 'none';
-    theme.style.display = 'none';
-    language.style.display = 'none';
-    security.style.display = 'none';
-});
-
-language.addEventListener("click", () => {
-
-    account.style.display = 'none';
-    theme.style.display = 'none';
-    language.style.display = 'none';
-    security.style.display = 'none';
-
-    portuguese.style.display = 'block';
-    english.style.display = 'block';
-    portugueseLanguage.style.display = 'block';
-    englishLanguage.style.display = 'block';
-});
+};
