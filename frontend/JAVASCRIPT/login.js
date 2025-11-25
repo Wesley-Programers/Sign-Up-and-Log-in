5746b11e7bf2e9f232508cdace95909fc04dd865
Wesley <123242004+Wesley-Programers@users.noreply.github.com>
2025-11-25 15:53:20 -0300

    Update login.js

--- frontend/JAVASCRIPT/login.js
+++ frontend/JAVASCRIPT/login.js
@@ -1,12 +1,82 @@
 document.addEventListener("DOMContentLoaded", () => {
+    let form = document.getElementById("formLogIn");
+    let button = document.getElementById("signInButton");
 
+    let incorrectName = document.getElementById("incorrectName");
+    let incorrectEmail = document.getElementById("incorrectEmail");
+    let incorrectPassword = document.getElementById("incorrectPassword");
+    
+
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target)
+
+        fetch("/main")
+            .then(res => res.json())
+            .then(data => {
+                alert(data.email)
+            });
+
+        try {
+
+            const fetchLogin = await fetch("", {
+                method: "POST",
+                body: formData
+            })
+
+            const status = fetchLogin.status
+            const mensagem = await fetchLogin.text()
+            alert(`Status: ${status} Mensagem: ${mensagem}`);
+
+            if (status === 200 && mensagem === "Dados de login validos") {
+
+                console.log("Is everything alright");
+
+                incorrectName.style.display = 'none';
+                incorrectEmail.style.block = 'none';
+                incorrectPassword.style.display = 'none';
+                setTimeout(() => {
+                    window.location.href = '../HTML/mainAccount.html'
+                }, 500);
+
+            } else {
+                e.preventDefault();
+
+                if (status === 409 && mensagem === "Nome incorreto") {
+                    console.log("nome incorreto");
+                    incorrectName.style.display = 'block';
+
+                    incorrectEmail.style.block = 'none';
+                    incorrectPassword.style.display = 'none';
+                } else if (status === 409 && mensagem === "Senha incorreta") {
+                    console.log("senha incorreta");
+                    incorrectPassword.style.display = 'block';
+
+                    incorrectName.style.display = 'none';
+                    incorrectEmail.style.display = 'none';
+                } else if (status === 409 && mensagem === "Email incorreto") {
+                    console.log("email incorreto");
+                    incorrectEmail.style.display = 'block';
+
+                    incorrectName.style.display = 'none';
+                    incorrectPassword.style.display = 'none';
+                };
+            };
+
+        } catch(error) {
+            console.error("ERROR: ", error);
+            alert("SOME ERROR");
+        };
+    });
 });
+
 
 function createAccount() {
     setTimeout(() => {
-        location.href = './index.html';
-    }, 250)
+        location.href = '../HTML/index.html';
+    }, 350);
 };
+
 
 function newLanguage() {
 
@@ -22,6 +92,9 @@ function newLanguage() {
     let forgotPassword = document.getElementById('forgotPassword');
     let inputNameEmail = document.getElementById('inputNameEmail');
     let passwordInput = document.getElementById('inputPassword');
+    let incorrectName = document.getElementById("incorrectName");
+    let incorrectEmail = document.getElementById("incorrectEmail");
+    let incorrectPassword = document.getElementById("incorrectPassword");
 
 
     if (language === "portuguese") {
@@ -35,6 +108,9 @@ function newLanguage() {
         inputNameEmail.placeholder = 'Nome ou Email';
         passwordInput.placeholder = 'Senha';
         dontHaveAccount.innerHTML = 'Não tem uma conta?';
+        incorrectName.innerHTML = 'Nome incorreto';
+        incorrectEmail.innerHTML = 'Email incorreto';
+        incorrectPassword.innerHTML = 'Senha incorreta';
 
     } else if (language === "english") {
 
@@ -47,6 +123,9 @@ function newLanguage() {
         inputNameEmail.placeholder = 'Name or Email';
         passwordInput.placeholder = 'Password';
         dontHaveAccount.innerHTML = "Don't have an account?"
+        incorrectName.innerHTML = 'Incorrect Name';
+        incorrectEmail.innerHTML = 'Incorrect Email';
+        incorrectPassword.innerHTML = 'Incorrect Password';
 
     };
 
