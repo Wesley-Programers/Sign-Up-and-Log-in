7e153357673f1f3aa0a2f36bb61c86e081e5fe60
Wesley <123242004+Wesley-Programers@users.noreply.github.com>
2026-01-17 19:05:42 -0300

    Create security.js

---
+++ frontend/JAVASCRIPT/security.js
@@ -0,0 +1,8 @@
+const data = new Date();
+const day = String(data.getDate());
+const month = String(data.getMonth() + 1);
+const year = data.getFullYear();
+
+const hours = String(data.getHours());
+const minutes = String(data.getMinutes());
+const seconds = String(data.getSeconds());
