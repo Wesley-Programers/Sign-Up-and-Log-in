55ba2b7c99dbb46887acef69ec6b04fa3fa855df
Wesley <123242004+Wesley-Programers@users.noreply.github.com>
2026-01-18 18:22:13 -0300

    Update login.js

--- frontend/JAVASCRIPT/login.js
+++ frontend/JAVASCRIPT/login.js
@@ -1,10 +1,192 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let form = document.getElementById("formLogIn");
-    let button = document.getElementById("signInButton");
 
-    let incorrectName = document.getElementById("incorrectName");
-    let incorrectEmail = document.getElementById("incorrectEmail");
-    let incorrectPassword = document.getElementById("incorrectPassword");
+    function teste(setItem, array) {
+
+        if (month < 10 && day > 10) {
+
+            if (hours < 10 && minutes > 10 && seconds > 10) {
+                let newHours = `0${hours}`;
+                let newMonth = `0${month}`;
+                const logMessage = ` ${day}/${newMonth}/${year} As ${newHours}:${minutes}:${seconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else if (hours > 10 && minutes < 10 && seconds > 10) {
+                let newMinutes = `0${minutes}`;
+                let newMonth = `0${month}`;
+                const logMessage = ` ${day}/${newMonth}/${year} As ${hours}:${newMinutes}:${seconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else if (hours > 10 && minutes > 10 && seconds < 10) {
+                let newSeconds = `0${seconds}`;
+                let newMonth = `0${month}`;
+                const logMessage = ` ${day}/${newMonth}/${year} As ${hours}:${minutes}:${newSeconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else if (hours < 10 && minutes < 10 && seconds > 10) {
+                let newHours = `0${hours}`;
+                let newMinutes = `0${minutes}`;
+                let newMonth = `0${month}`;
+                const logMessage = ` ${day}/${newMonth}/${year} As ${newHours}:${newMinutes}:${seconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else if (hours > 10 && minutes < 10 && seconds < 10) {
+                let newSeconds = `0${seconds}`;
+                let newMinutes = `0${minutes}`;
+                let newMonth = `0${month}`;
+                const logMessage = ` ${day}/${newMonth}/${year} As ${hours}:${newMinutes}:${newSeconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else if (hours < 10 && minutes > 10 && seconds < 10) {
+                let newHours = `0${hours}`;
+                let newSeconds = `0${seconds}`;
+                let newMonth = `0${month}`;
+                const logMessage = ` ${day}/${newMonth}/${year} As ${newHours}:${minutes}:${newSeconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else {
+                let newMonth = `0${month}`;
+                const logMessage = ` ${day}/${newMonth}/${year} As ${hours}:${minutes}:${seconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            }
+            
+        } else if (day < 10 && month > 10) {
+            
+            if (hours < 10 && minutes > 10 && seconds > 10) {
+                let newHours = `0${hours}`;
+                let newDay = `0${day}`
+                const logMessage = ` ${newDay}/${month}/${year} As ${newHours}:${minutes}:${seconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else if (hours > 10 && minutes < 10 && seconds > 10) {
+                let newMinutes = `0${minutes}`;
+                let newDay = `0${day}`
+                const logMessage = ` ${newDay}/${month}/${year} As ${hours}:${newMinutes}:${seconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else if (hours > 10 && minutes > 10 && seconds < 10) {
+                let newSeconds = `0${seconds}`;
+                let newDay = `0${day}`
+                const logMessage = ` ${newDay}/${month}/${year} As ${hours}:${minutes}:${newSeconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else if (hours < 10 && minutes < 10 && seconds > 10) {
+                let newHours = `0${hours}`;
+                let newMinutes = `0${minutes}`;
+                let newDay = `0${day}`
+                const logMessage = ` ${newDay}/${month}/${year} As ${newHours}:${newMinutes}:${seconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else if (hours > 10 && minutes < 10 && seconds < 10) {
+                let newSeconds = `0${seconds}`;
+                let newMinutes = `0${minutes}`;
+                let newDay = `0${day}`
+                const logMessage = ` ${newDay}/${month}/${year} As ${hours}:${newMinutes}:${newSeconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else if (hours < 10 && minutes > 10 && seconds < 10) {
+                let newHours = `0${hours}`;
+                let newSeconds = `0${seconds}`;
+                let newDay = `0${day}`
+                const logMessage = ` ${newDay}/${month}/${year} As ${newHours}:${minutes}:${newSeconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else {
+                let newDay = `0${day}`
+                const logMessage = ` ${newDay}/${month}/${year} As ${hours}:${minutes}:${seconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            }
+
+        } else if (day > 10 && month > 10) {
+
+            if (hours < 10 && minutes > 10 && seconds > 10) {
+                let newHours = `0${hours}`;
+                const logMessage = ` ${day}/${month}/${year} As ${newHours}:${minutes}:${seconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else if (hours > 10 && minutes < 10 && seconds > 10) {
+                let newMinutes = `0${minutes}`;
+                const logMessage = ` ${day}/${month}/${year} As ${hours}:${newMinutes}:${seconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else if (hours > 10 && minutes > 10 && seconds < 10) {
+                let newSeconds = `0${seconds}`;
+                const logMessage = ` ${day}/${month}/${year} As ${hours}:${minutes}:${newSeconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, setarrayItem);
+
+            } else if (hours < 10 && minutes < 10 && seconds > 10) {
+                let newHours = `0${hours}`;
+                let newMinutes = `0${minutes}`;
+                const logMessage = ` ${day}/${month}/${year} As ${newHours}:${newMinutes}:${seconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else if (hours > 10 && minutes < 10 && seconds < 10) {
+                let newSeconds = `0${seconds}`;
+                let newMinutes = `0${minutes}`;
+                const logMessage = ` ${day}/${month}/${year} As ${hours}:${newMinutes}:${newSeconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else if (hours < 10 && minutes > 10 && seconds < 10) {
+                let newHours = `0${hours}`;
+                let newSeconds = `0${seconds}`;
+                const logMessage = ` ${day}/${month}/${year} As ${newHours}:${minutes}:${newSeconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+
+            } else {
+                const logMessage = ` ${day}/${month}/${year} As ${hours}:${minutes}:${seconds}`;
+                array.push(logMessage);
+                localStorage.setItem(`${setItem}`, array);
+            };
+
+        };
+    };
+
+    let logs = [];
+    let lastLogin = [];
+
+    const data = new Date();
+    const day = String(data.getDate());
+    const month = String(data.getMonth() + 1);
+    const year = data.getFullYear();
+
+    const hours = String(data.getHours());
+    const minutes = String(data.getMinutes());
+    const seconds = String(data.getSeconds());
+
+
+    let form = document.getElementById("");
+    let button = document.getElementById("");
+
+    let incorrectName = document.getElementById("");
+    let incorrectEmail = document.getElementById("");
+    let incorrectPassword = document.getElementById("");
+
+    let dontHaveAccount = document.getElementById("");
+
+    dontHaveAccount.addEventListener("click", () => {
+        window.location.href = '../HTML/index.html'
+    });
     
 
     form.addEventListener("submit", async (e) => {
@@ -13,32 +195,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
         try {
 
-            fetch("", {
-                method: "GET",
-                credentials: "include",
-            })
-            .then(async res => {
-                if (!res.ok) {
-                    const text = await res.text();
-                    throw new Error("ERROR HTTP " + res.status + ": " + text)
-                }
-                return res.json();
-            })
-            .then(data => alert(data.email))
-            .catch(err => console.error(err));
-
-        } catch(error) {
-
-            console.error("", error);
-            alert("");
-
-        };
-
-
-        try {
-
             const fetchLogin = await fetch("", {
                 method: "POST",
+                credentials: "include",
                 body: formData
             })
 
@@ -48,13 +207,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if (status === 200 && mensagem === "") {
 
-                console.log("");
+                teste("lastLogin", lastLogin);
+
+                console.log("Is everything alright");
 
                 incorrectName.style.display = 'none';
-                incorrectEmail.style.block = 'none';
+                incorrectEmail.style.display = 'none';
                 incorrectPassword.style.display = 'none';
+
                 setTimeout(() => {
-                    window.location.href = ''
+                    window.location.href = '../HTML/mainAccount.html'
                 }, 500);
 
             } else {
@@ -64,11 +226,15 @@ document.addEventListener("DOMContentLoaded", () => {
                     console.log("nome incorreto");
                     incorrectName.style.display = 'block';
 
+                    teste("logs", logs);
+
                     incorrectEmail.style.block = 'none';
                     incorrectPassword.style.display = 'none';
                 } else if (status === 409 && mensagem === "") {
                     console.log("senha incorreta");
                     incorrectPassword.style.display = 'block';
+
+                    teste("logs", logs);
 
                     incorrectName.style.display = 'none';
                     incorrectEmail.style.display = 'none';
@@ -76,6 +242,8 @@ document.addEventListener("DOMContentLoaded", () => {
                     console.log("email incorreto");
                     incorrectEmail.style.display = 'block';
 
+                    teste("logs", logs);
+                    
                     incorrectName.style.display = 'none';
                     incorrectPassword.style.display = 'none';
                 };
@@ -87,64 +255,3 @@ document.addEventListener("DOMContentLoaded", () => {
         };
     });
 });
-
-
-function createAccount() {
-    setTimeout(() => {
-        location.href = '../HTML/index.html';
-    }, 350);
-};
-
-
-function newLanguage() {
-
-    let languages = document.getElementById('languages');
-    let language = languages.value;
-
-    let button = document.getElementById('signInButton');
-    let portugueseLanguage = document.getElementById('portugueseLanguage');
-    let englishLanguage = document.getElementById('englishLanguage');
-    let welcomeBack = document.getElementById('welcomeBack');
-    let title = document.getElementById('signInTitle');
-    let dontHaveAccount = document.getElementById('dontHaveAccount');
-    let forgotPassword = document.getElementById('forgotPassword');
-    let inputNameEmail = document.getElementById('inputNameEmail');
-    let passwordInput = document.getElementById('inputPassword');
-    let incorrectName = document.getElementById("incorrectName");
-    let incorrectEmail = document.getElementById("incorrectEmail");
-    let incorrectPassword = document.getElementById("incorrectPassword");
-
-
-    if (language === "portuguese") {
-
-        button.innerHTML = 'Entrar';
-        portugueseLanguage.innerHTML = 'Português';
-        englishLanguage.innerHTML = 'Inglês';
-        welcomeBack.innerHTML = 'BEM VINDO DE VOLTA';
-        title.innerHTML = 'LOGIN';
-        forgotPassword.innerHTML = 'Esqueceu a senha?';
-        inputNameEmail.placeholder = 'Nome ou Email';
-        passwordInput.placeholder = 'Senha';
-        dontHaveAccount.innerHTML = 'Não tem uma conta?';
-        incorrectName.innerHTML = 'Nome incorreto';
-        incorrectEmail.innerHTML = 'Email incorreto';
-        incorrectPassword.innerHTML = 'Senha incorreta';
-
-    } else if (language === "english") {
-
-        button.innerHTML = 'Log in';
-        portugueseLanguage.innerHTML = 'Portuguese';
-        englishLanguage.innerHTML = 'English';
-        welcomeBack.innerHTML = 'WELCOME BACK';
-        title.innerHTML = 'LOG IN';
-        forgotPassword.innerHTML = 'Forgot the password?';
-        inputNameEmail.placeholder = 'Name or Email';
-        passwordInput.placeholder = 'Password';
-        dontHaveAccount.innerHTML = "Don't have an account?"
-        incorrectName.innerHTML = 'Incorrect Name';
-        incorrectEmail.innerHTML = 'Incorrect Email';
-        incorrectPassword.innerHTML = 'Incorrect Password';
-
-    };
-
-};
