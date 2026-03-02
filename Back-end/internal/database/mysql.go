f2f804471333230123c748480d9aad510b7c91f5
Wesley-Programers <wesleyprogrammers@gmail.com>
2026-03-02 18:07:17 -0300

    feat: add initial database schema

--- Back-end/internal/database/mysql.go
+++ Back-end/internal/database/mysql.go
@@ -17,8 +17,6 @@ func Connect() *sql.DB {
 		log.Fatal("ERROR: ", err)
 	}
 
-	// log.SetFlags(log.Lshortfile)
-
 	databaseUser := os.Getenv("DATABASE_USER")
 	databasePassword := os.Getenv("DATABASE_PASSWORD")
 	databaseName := os.Getenv("DATABASE_NAME")
@@ -37,20 +35,19 @@ func Connect() *sql.DB {
     }
 
 	CreateTable(database)
+	CreateTableResetPassword(database)
+	CreateTableAttempts(database)
 	return database
-
 }
 
 
-func CreateTable(db *sql.DB) {
+func CreateTable(database *sql.DB) {
 
-	// log.SetFlags(log.Lshortfile)
-
-	if db == nil {
-		log.Fatal("ERROR ON SQL TABLE") 
+	if database == nil {
+		log.Fatal("ERROR") 
 	}
 
-	_, err := db.Exec(`
+	_, err := database.Exec(`
 	CREATE TABLE IF NOT EXISTS users (
 		id INT AUTO_INCREMENT PRIMARY KEY,
 		name VARCHAR(50) NOT NULL,
@@ -62,5 +59,47 @@ func CreateTable(db *sql.DB) {
 	if err != nil {
 		log.Fatal("ERROR: ", err)
 	}
+}
 
+
+func CreateTableResetPassword(database *sql.DB) {
+
+	if database == nil {
+		log.Fatal("ERROR")
+	}
+
+	_, err := database.Exec(`
+	CREATE TABLE IF NOT EXISTS reset_password (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		user_id INT NOT NULL,
+		token VARCHAR(255) UNIQUE NOT NULL,
+		created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		expires_at TIMESTAMP NOT NULL,
+		used BOOLEAN DEFAULT FALSE,
+		FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCATE
+	)`)
+
+	if err != nil {
+		log.Fatal("ERROR: ", err)
+	}
+}
+
+
+func CreateTableAttempts(database *sql.DB) {
+
+	if database == nil {
+		log.Fatal("ERROR")
+	}
+
+	_, err := database.Exec(`
+	CREATE TABLE IF NOT EXISTS attempts (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		email VARCHAR(120) NOT NULL,
+		attempted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		INDEX index_email_time(email, attempted_at)
+	)`)
+
+	if err != nil {
+		log.Fatal("ERROR: ", err)
+	}
 }
