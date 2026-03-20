5100b2058bd854c710403561a068adab08745894
Wesley-Programers <wesleyprogrammers@gmail.com>
2026-03-20 18:26:59 -0300

    feat: add env loading and configure CORS middleware

--- Back-end/cmd/main.go
+++ Back-end/cmd/main.go
@@ -2,51 +2,62 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"index/Back-end/internal/database"
 	"index/Back-end/internal/handlers"
-	"index/Back-end/internal/service"
 	"index/Back-end/internal/repository"
+	"index/Back-end/internal/service"
+
+	"github.com/joho/godotenv"
 )
 
 
 func main() {
-	database := database.Connect()
-	defer database.Close()
 
-	go service.StartToRemoverExpiredTokens()
+	if err := godotenv.Load(); err != nil {
+		log.Println("Warning: file .env was not found")
+	}
+	jwtKey := os.Getenv("JWT_KEY")
+
+	db := database.Connect()
+	defer db.Close()
+	// database.RunMigrations(db)
+
+	go service.StartToRemoverExpiredTokens(db)
 	mux := http.NewServeMux()
 	
-	repositoryRegister := repository.NewRegisterStruct(database)
+	repositoryRegister := repository.NewRegisterStruct(db)
 	serviceRegister := service.NewUserStruct(repositoryRegister)
 	handlerRegister := handlers.NewRegisterHanlder(serviceRegister)
 
-	repositoryLogin := repository.NewVerifyLoginStruct(database)
+	repositoryLogin := repository.NewVerifyLoginStruct(db)
 	serviceLogin := service.NewVerifyLogin(repositoryLogin)
 	handlerLogin := handlers.NewLoginHandler(serviceLogin)
 
-	repositoryChangeName := repository.NewChangeNameStruct(database)
+	repositoryChangeName := repository.NewChangeNameStruct(db)
 	serviceChangeName := service.NewChangeName(repositoryChangeName)
 	handlerChangeName := handlers.NewChangeNameHandler(serviceChangeName)
 
-	repositoryChangeEmail := repository.NewChangeEmailStruct(database)
+	repositoryChangeEmail := repository.NewChangeEmailStruct(db)
 	serviceChangeEmail := service.NewChangeEmail(repositoryChangeEmail)
 	handlerChangeEmail := handlers.NewChangeEmailHandler(serviceChangeEmail)
 
-	repositoryRequest := repository.NewRequestStruct(database)
+	repositoryRequest := repository.NewRequestStruct(db)
 	serviceRequest := service.NewRequest(repositoryRequest)
 	handlerRequest := handlers.NewRequestHandler(serviceRequest)
 
-	repositoryResetPassword := repository.NewResetPasswordStruct(database)
+	repositoryResetPassword := repository.NewResetPasswordStruct(db)
 	serviceResetPassword := service.NewResetPassword(repositoryResetPassword)
 	handlerResetPassword := handlers.NewResetPasswordHandler(serviceResetPassword)
 
-	repositoryDeleteAccount := repository.NewDeleteAccountStruct(database)
+	repositoryDeleteAccount := repository.NewDeleteAccountStruct(db)
 	serviceDeleteAccount := service.NewDeleteAccount(repositoryDeleteAccount)
 	handlerDeleteAccount := handlers.NewDeleteAccountHandler(serviceDeleteAccount)
 
-	repositoryValidToken := repository.NewValidTokenStruct(database)
+	repositoryValidToken := repository.NewValidTokenStruct(db)
 	serviceValidToken := service.NewValidToken(repositoryValidToken)
 	handlerValidToken := handlers.NewValidTokenHandler(serviceValidToken)
 
@@ -55,8 +66,10 @@ func main() {
 	mux.HandleFunc("/login", handlerLogin.HandlerLogin)
 
 	mux.HandleFunc("/change", handlerChangeName.ChangeNameHandler)
-	mux.HandleFunc("/email", handlerChangeEmail.ChangeEmailHandler)
 
+	getEnvMiddleware := handlers.AuthMiddleware(jwtKey)
+	mux.Handle("/email", getEnvMiddleware(http.HandlerFunc(handlerChangeEmail.ChangeEmailHandler)))
+	
 	mux.HandleFunc("/delete", handlerDeleteAccount.DeleteAccountHandler)
 
 	mux.HandleFunc("/reset", handlerRequest.RequestHandler)
@@ -64,7 +77,8 @@ func main() {
 
 	mux.HandleFunc("/valid", handlerValidToken.ValidTokenHandler)
 
-	middleware := handlers.Recovery(mux)
+	testHandler := handlers.Recovery(mux)
+	middleware := handlers.CorsMiddleware(testHandler)
 	
 	fmt.Println("SERVER OPEN WITH GOLANG")
 	http.ListenAndServe("127.0.0.1:8000", middleware)
