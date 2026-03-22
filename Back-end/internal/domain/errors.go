0ece790a26d971e9021b172bfb279ea0fa2a613d
Wesley-Programers <wesleyprogrammers@gmail.com>
2026-03-22 18:54:44 -0300

    feat: add new domain error definitions

--- Back-end/internal/domain/errors.go
+++ Back-end/internal/domain/errors.go
@@ -5,6 +5,11 @@ import "errors"
 var (
 	ErrUserNotFound = errors.New("User not found")
 	ErrEmailAlreadyExist = errors.New("Email already exist")
-	ErrInvaliData = errors.New("Invalid input")
+	ErrInvalidData = errors.New("Invalid input")
 	ErrInternal = errors.New("Internal error occurred")
+	ErrInvalidPassword = errors.New("Invalid password")
+	ErrInvalidCredentials = errors.New("Invalid email, name or password")
+	ErrNameIsTheSame = errors.New("The new name is the same as the current one")
+	ErrEmailIsTheSame = errors.New("The new email is the same as the current one")
+	ErrEmailMismatch = errors.New("The new email and its confirmation are not the same")
 )
