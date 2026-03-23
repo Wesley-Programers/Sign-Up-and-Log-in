9c1b383562cdc73db44e32ef8f32941dbfa8ce4a
Wesley-Programers <wesleyprogrammers@gmail.com>
2026-03-23 17:14:28 -0300

    feat(domain): implement robust identify and validation logic in User entity
    - Added Login to handle both email and username identification

--- Back-end/internal/domain/user.go
+++ Back-end/internal/domain/user.go
@@ -7,59 +7,100 @@ import (
 )
 
 type User struct {
-	id int
-	name string
-	email string
+	Id int
+	Name string
+	Email string
 	PasswordHash string
 }
 
-func (user *User) ID() int { return user.id }
-func (user *User) Name() string { return user.name }
-func (user *User) Email() string { return user.email }
+func (user *User) ID() int { return user.Id }
+func (user *User) NAME() string { return user.Name }
+func (user *User) EMAIL() string { return user.Email }
+
 
 func (user *User) ChangeEmail(currentEmail, newEmail, confirmNewEmail string) error {
 	
-	if !strings.EqualFold(currentEmail, user.email) {
+	if newEmail != confirmNewEmail {
 		return ErrEmailMismatch
 	}
 
-	if strings.EqualFold(newEmail, user.email) {
+	if !strings.EqualFold(currentEmail, user.Email) {
+		return ErrEmailMismatch
+	}
+
+	if strings.EqualFold(newEmail, user.Email) {
 		return ErrEmailIsTheSame
 	}
 
-	user.email = strings.ToLower(newEmail)
+	if !strings.HasSuffix(newEmail, "@example.com") {
+		return ErrInvalidEmailFormat
+	}
+
+	user.Email = strings.ToLower(newEmail)
 	return nil
 }
+
 
 func (user *User) ChangeName(currentName, newName, confirmNewEmail string) error {
 
-	if !strings.EqualFold(currentName, user.name) {
+	if !strings.EqualFold(currentName, user.Name) {
 		return ErrUserNotFound
 	}
 
-	if strings.EqualFold(newName, user.name) {
+	if strings.EqualFold(newName, user.Name) {
 		return ErrNameIsTheSame
 	}
 
-	user.name = strings.ToLower(newName)
+	user.Name = strings.ToLower(newName)
 	return nil
 }
 
-func (user *User) PasswordValid(password string) bool {
-	return bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password)) == nil
+
+func (user *User) Login(nameOrEmail, password string) error {
+
+	isEmail := strings.HasSuffix(nameOrEmail, "@example.com")
+	if isEmail {
+		if !strings.EqualFold(nameOrEmail, user.Email) {
+			return ErrUserNotFound
+		}
+
+	} else {
+		if !strings.EqualFold(nameOrEmail, user.Name) {
+			return ErrUserNotFound
+		}
+	}
+
+	return nil
 }
+
+
+func (user *User) PasswordValid(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+}
+
 
 func Restore(id int, email, passwordHash string) *User {
 	return &User{
-		id: id,
-		email: email,
+		Id: id,
+		Email: email,
 		PasswordHash: passwordHash,
 	}
 }
 
+
 func RestoreName(id int, name string) *User {
 	return &User{
-		id: id,
-		name: name,
+		Id: id,
+		Name: name,
+	}
+}
+
+
+func RestoreLogin(id int, name, email, passwordHash string) *User {
+	return &User{
+		Id: id,
+		Name: name,
+		Email: email,
+		PasswordHash: passwordHash,
 	}
 }
