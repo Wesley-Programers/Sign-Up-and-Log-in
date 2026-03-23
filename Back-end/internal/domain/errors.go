196b04a48bf908418cacc1d7f510d4f22f7f6f75
Wesley-Programers <wesleyprogrammers@gmail.com>
2026-03-23 17:10:53 -0300

    feat(domain): refactor error definitions for security and consistency
    - Grouped errors by category (Identify, Validation, Business, Infra). - Updated error messages to prevent user enumeration. - Added ErrRateLimitExceeded for Redis integration.

--- Back-end/internal/domain/errors.go
+++ Back-end/internal/domain/errors.go
@@ -3,13 +3,19 @@ package domain
 import "errors"
 
 var (
-	ErrUserNotFound = errors.New("User not found")
-	ErrEmailAlreadyExist = errors.New("Email already exist")
-	ErrInvalidData = errors.New("Invalid input")
-	ErrInternal = errors.New("Internal error occurred")
-	ErrInvalidPassword = errors.New("Invalid password")
-	ErrInvalidCredentials = errors.New("Invalid email, name or password")
-	ErrNameIsTheSame = errors.New("The new name is the same as the current one")
-	ErrEmailIsTheSame = errors.New("The new email is the same as the current one")
-	ErrEmailMismatch = errors.New("The new email and its confirmation are not the same")
+	ErrInvalidCredentials = errors.New("invalid email, name or password")
+	ErrUserNotFound = errors.New("user not found")
+	ErrEmailAlreadyExists = errors.New("email already exist")
+	ErrInvalidPassword = errors.New("invalid password")
+
+	ErrInvalidData = errors.New("invalid input")
+	ErrInvalidEmailFormat = errors.New("invalid email format")
+
+	ErrNameIsTheSame = errors.New("the new name is the same as the current one")
+	ErrEmailIsTheSame = errors.New("the new email is the same as the current one")
+	ErrEmailMismatch = errors.New("the provided current email does not match our records")
+	ErrEmailsDoNotMismatch = errors.New("the new email and its confirmation do not match")
+
+	ErrInternal = errors.New("an internal error occurred")
+	ErrRateLimitExceeded = errors.New("too many requests, please try again later")
 )
